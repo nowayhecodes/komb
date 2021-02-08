e37900f85e182a06b8b34d046b3c8137f7649fbd
Gustavo Cavalcante <nowayhecodes@gmail.com>
2021-02-08 01:23:15 -0300

    Eval combinations

--- src/main/scala/komb/Interpreter.scala
+++ src/main/scala/komb/Interpreter.scala
@@ -14,9 +14,19 @@ case class Proc(f: ((Env, List[ExprT]) => (Env, ExprT))) extends ExprT
 case class Symbol(v: String) extends ExprT
 case class Value(v: ValueT) extends ExprT
 
-
 object Interpreter {
   def eval(env: Env, expr: ExprT): (Env, ExprT) = expr match {
     case NullExpr() => throw new IllegalStateException("invalid interpreter state")
+    case Comb(List()) =>throw new IllegalStateException("invalid combination")
+    case Comb(h :: t) =>
+      eval(env, h) match {
+        case (_, Proc(f)) => apply(f, t, env)
+        case (nEnv, Func(args, body)) => {
+          if (args.length != t.length) throw new IllegalArgumentException("invalid number of arguments")
+          val newEnv = (args zip t).foldLeft(nEnv.expand())((acc, av) => bindargs(acc, av._1, av._2))
+          evalAll(newEnv, body)
+        }
+        case (newEnv, expr) => (nEnv, expr)
+      }
   }
 }
