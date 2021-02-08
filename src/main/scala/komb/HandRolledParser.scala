7da6a521105dbf656348e6818d15ca81d9b5e2a9
Gustavo Cavalcante <nowayhecodes@gmail.com>
2021-02-08 16:36:41 -0300

    finish HandRolledParser

--- src/main/scala/komb/HandRolledParser.scala
+++ src/main/scala/komb/HandRolledParser.scala
@@ -15,6 +15,27 @@ case class TString(v: String) extends Token
 case class TSymbol(v: String) extends Token
 
 object HandRolledParser {
+  def parse(source: String): Unit = {
+    def mapToken(token: Token) = token match {
+      case TNumber(n) => Value(Num(BigDecimal(n)))
+      case TString(s) => Value(Name(s))
+      case TSymbol(sym) => Symbol(sym)
+      case _ => throw new IllegalArgumentException("syntax error")
+    }
+
+    def doParse(acc: List[ExprT], tokens: List[Token]): (List[ExprT], List[Token]) = tokens match {
+      case TOpen() :: t =>
+        val (c, rst) = doParse(List(), t)
+        doParse(Comb(c) :: acc, rst)
+      case TClose() :: t => (acc.reverse, t)
+      case h :: t => doParse(mapToken(h) :: acc, t)
+      case List() => (acc.reverse, List())
+    }
+
+    val (res, _) = doParse(List(), (tokenize(source.toList)))
+    res
+  }
+
   def tokenize(source: List[Char]): Unit = {
     @tailrec
     def string(acc: String, cs: List[Char]): (String, List[Char]) = cs match {
