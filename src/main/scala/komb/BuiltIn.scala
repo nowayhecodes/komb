913a74a8fe5d0187b2abf7ea7a15f3346feff3c3
Gustavo Cavalcante <nowayhecodes@gmail.com>
2021-02-09 15:01:48 -0300

    More builtins

--- src/main/scala/komb/BuiltIn.scala
+++ src/main/scala/komb/BuiltIn.scala
@@ -2,6 +2,8 @@ package komb
 
 import komb.Interpreter._
 
+import scala.annotation.tailrec
+
 object BuiltIn {
   def aritFn(op: ((BigDecimal, BigDecimal) => BigDecimal))
             (env: Env, comb: List[ExprT]): (Env, Value) = {
@@ -40,4 +42,89 @@ object BuiltIn {
     case Value(v) :: t => v :: buildList(t)
     case _ => throw new IllegalArgumentException("define args")
   }
+
+  def listToString(ls: List[ExprT]): String = {
+    def convert(ls: List[ExprT]): String = ls match {
+      case List() => ""
+      case Value(Num(v)) :: t => v.toString + ", " + convert(t)
+      case Value(Bool(v)) :: t => v.toString + ", " + convert(t)
+      case Value(Name(v)) :: t => v + ", " + convert(t)
+      case EList(l) :: t => "(" + convert(l) + "), " + convert(t)
+      case _ :: t => convert(t)
+    }
+
+    "(" + convert(ls) + ")"
+  }
+
+  def _not(env: Env, comb: List[ExprT]): (Env, Value) = comb match {
+    case expr :: Nil => eval(env, expr) match {
+      case (_, Value(Bool(v))) => (env, Value(Bool(!v)))
+      case _ => throw new IllegalArgumentException("not")
+    }
+    case _ => throw new IllegalArgumentException("not")
+  }
+
+  def _if(env: Env, comb: List[ExprT]): (Env, ExprT) = {
+    val (condExpr, posExpr, negExpr) = comb match {
+      case condExpr :: posExpr :: negExpr :: Nil => (condExpr, posExpr, Some(negExpr))
+      case condExpr :: posExpr :: Nil => (condExpr, posExpr, None)
+      case _ => throw new IllegalArgumentException("if")
+    }
+    (eval(env, condExpr))._2 match {
+      case Value(Bool(c)) =>
+        if (c)
+          eval(env, posExpr)
+        else negExpr match {
+          case Some(e) => eval(env, e)
+          case None => (env, NullExpr())
+        }
+      case _ => throw new IllegalArgumentException("if")
+    }
+  }
+
+  def _cond(env: Env, comb: List[ExprT]): Unit = {
+    def doExpression(comb: List[ExprT]) = comb match {
+      case Symbol("else") :: posExpr :: Nil => Some(eval(env, posExpr)._2)
+      case condExpr :: posExpr :: Nil => eval(env, condExpr)._2 match {
+        case Value(Bool(true)) => Some(eval(env, posExpr)._2)
+        case Value(Bool(false)) => None
+        case _ => throw new IllegalArgumentException("cond")
+      }
+      case _ => throw new IllegalArgumentException("cond")
+    }
+
+    @tailrec
+    def runExpressions(comb: List[ExprT]): ExprT = comb match {
+      case Comb(c) :: rst => doExpression(c) match {
+        case Some(e) => e
+        case None => runExpressions(rst)
+      }
+
+      case _ => NullExpr()
+    }
+
+    (env, runExpressions(comb))
+  }
+
+  def _define(env: Env, comb: List[ExprT]): (Env, NullExpr) = {
+    def getString(expr: ExprT) = expr match {
+      case Symbol(sym) => sym
+      case Value(Name(n)) => n
+      case _ => throw new IllegalArgumentException("define")
+    }
+    comb match {
+      case Symbol(sym) :: expr :: Nil => {
+        val (nEnv, res) = eval(env, expr)
+        (nEnv.addEntry(sym -> res), NullExpr())
+      }
+      case Comb(ns) :: body => {
+        val fName = getString(ns.head)
+        val args = buildList(ns.tail)
+        (env.addEntry(fName -> Func(args, body)), NullExpr())
+      }
+      case _ => throw new IllegalArgumentException("define")
+    }
+  }
+
+  
 }
